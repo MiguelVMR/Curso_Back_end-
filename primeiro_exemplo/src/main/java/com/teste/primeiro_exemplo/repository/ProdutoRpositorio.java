b3d1c20a7ff9f63df2cd70f8be4a841a6b4954d3
MiguelVilela <miguelmvilela12@gmail.com>
2023-03-10 16:15:07 -0300

    Criado o método de atualização do produto

--- primeiro_exemplo/src/main/java/com/teste/primeiro_exemplo/repository/ProdutoRpositorio.java
+++ primeiro_exemplo/src/main/java/com/teste/primeiro_exemplo/repository/ProdutoRpositorio.java
@@ -1,6 +1,7 @@
 package com.teste.primeiro_exemplo.repository;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Optional;
 
@@ -50,6 +51,27 @@ public class ProdutoRpositorio {
      */
     public void deletar (Integer id){
         produtos.removeIf(produto -> produto.getId() == id);
+
+    }
+    /**
+     * Metodo para atualziar o produto na lista 
+     * @param produto que sera atualizado
+     * @return Reporna o produto apos atualziar a lista 
+     */
+    public Produto atualizar(Produto produto){
+        // Encontrar o produto na lista.
+        Optional<Produto> produtoEncontrado = obterPorId(produto.getId());
+        if(produtoEncontrado.isEmpty()){
+            throw new InputMismatchException("Produto não encontrado");
+        }
+
+        //Eu tenho que remover o produto antigo da lista. 
+        deletar(produto.getId());
+        
+        // Depois adcionar o produtudo atualziado na lista
+        produtos.add(produto);
+
+        return produto;
 
     }
 }
