d4951d6e86b0fd7d9cdf041f2ea7144ff8c95b60
MiguelVilela <miguelmvilela12@gmail.com>
2023-03-10 17:51:30 -0300

    Criando o metodo obeter por id no controller

--- primeiro_exemplo/src/main/java/com/teste/primeiro_exemplo/controller/ProdutoController.java
+++ primeiro_exemplo/src/main/java/com/teste/primeiro_exemplo/controller/ProdutoController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,13 @@ public class ProdutoController {
     @GetMapping
     public List <Produto> ObterTodos(){
        return produtoService.obterTodos();
+    }
+    
+
+    @ PostMapping
+    public Produto adionar (@RequestBody  Produto produto ){
+        return produtoService.adicionar(produto);
+
     }
 
 
